370036cac8fd51127e7acb3ba43eea1901b65b40
adonide <116856748+adonide@users.noreply.github.com>
2022-12-19 01:40:55 +0000

    chg test2

--- test2.ts
+++ test2.ts
@@ -1,3 +1,12 @@
 class test2{
-    private a:number = 1;
+    private a:Number;
+    private arr:any[];
+
+    constructor(){
+        this.arr = [];
+        for (let i = 0; i < 10; i++) {
+            this.arr.push(i)
+        }
+    }
+
 }
